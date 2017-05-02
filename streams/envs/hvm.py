64935d9e408dcf05ec8441d5df4c767ffc32d1b8
Jonas Kubilius <qbilius@gmail.com>
2017-05-02 13:53:51 -0400

    generic hvm class

--- streams/envs/hvm.py
+++ streams/envs/hvm.py
@@ -48,7 +48,7 @@ class Dataset(object):
     def home(self, *suffix_paths):
         return os.path.join(DATA_HOME, self.name, *suffix_paths)
 
-    def datapath(self, handle):
+    def datapath(self, handle, prefix=None):
         data = self.DATA[handle]
         if isinstance(data, tuple):
             s3_path, sha1, local_path = data
@@ -56,9 +56,12 @@ class Dataset(object):
                 local_path = s3_path.replace(self.COLL + '/' + self.name + '/', '', 1)
         else:
             local_path = data.replace(self.COLL + '/' + self.name + '/', '', 1)
+        if prefix is not None:
+            local_path = '/'.join([prefix, local_path])
         return self.home(local_path)
 
     def fetch(self):
+        return
         if not os.path.exists(self.home()):
             os.makedirs(self.home())
 
@@ -133,6 +136,131 @@ class Dataset(object):
     #     client.delete_object(Bucket=self.BUCKET, Key=new_path)
 
 
+class HvM(Dataset):
+
+    DATA = {'meta': 'streams/hvm/meta.pkl',
+
+            'imageset/tfrecords/var0': 'streams/hvm/imageset/tfrecords/var0.tfrecords',
+            'imageset/tfrecords/var3': 'streams/hvm/imageset/tfrecords/var3.tfrecords',
+            'imageset/tfrecords/var6': 'streams/hvm/imageset/tfrecords/var6.tfrecords',
+            'imageset/tfrecords/meta': 'streams/hvm/imageset/tfrecords/meta.pkl',
+
+            # 'meta': ('streams/hvm/imageset/meta.pkl', None, 'hvm/imageset/meta.pkl'),
+
+            'neural/averaged/var0': ('Chabo_Tito_20110907_Var00a_pooled_P58.trim.wh.evoked.repr.h5',
+                              '300b4446797b6244987f3d98b38c4cb36f61086d', None),
+            'neural/averaged/var3': ('Chabo_Tito_20110907_Var03a_pooled_P58.trim.wh.evoked.repr.h5',
+                              'd72802384c35915ceee80a530e1d1053b086975a', None),
+            'neural/averaged/var6': ('Chabo_Tito_20110907_Var06a_pooled_P58.trim.wh.evoked.repr.h5',
+                              'a95c797b0b2eef56d431c8ccca4c160143a65357', None),
+
+            'neural/temporal/var0': ('Chabo_Tito_20140307_Var00a_pooled_P58.trim.wh.evoked.repr.h5',
+                              '588d6d118a45e98c65260e9226c237c72244af0d', None),
+            'neural/temporal/var3': ('Chabo_Tito_20140307_Var03a_pooled_P58.trim.wh.evoked.repr.h5',
+                              '52a35255b1595c29be747c5725be7d6f0e6bd037', None),
+            'neural/temporal/var6': ('Chabo_Tito_20140307_Var06a_pooled_P58.trim.wh.evoked.repr.h5',
+                              '8ed6ec266fd0104368121aa742038f04681f7231', None),
+            # 'temporal_raw': 'Chabo_Tito_20140307_Var06a_pooled_P58.trim.raw.d.repr.h5',
+            # 'temporal_evoked': 'Chabo_Tito_20140307_Var06a_pooled_P58.trim.evoked.repr.h5'
+
+            'model/alexnet/pool5': 'streams/hvm/model/alexnet_pool5_feats_pca1000.npy',
+            'model/hmo/layer1': 'streams/hvm/model/hmo_layer1feats.npy',
+            'model/hmo/layer2': 'streams/hvm/model/hmo_layer2feats.npy',
+            'model/hmo/layer3': 'streams/hvm/model/hmo_layer3feats.npy',
+            'model/hmo/top': 'streams/hvm/model/hmo_topfeats.npy',
+            }
+
+    IT_NEURONS = np.array([ 10, 121, 122, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134,
+                           135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147,
+                           148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160,
+                           161, 162, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231,
+                           232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244,
+                           245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255,   0,   1,
+                             2,   3,   4,   5,   6,   7,   8,   9,  56,  58,  59,  61,  63,
+                            65,  67,  68,  69,  71,  72,  73,  74,  75,  76,  77,  78,  79,
+                            80,  81,  82,  83,  84, 123, 124, 171, 172, 173, 174, 175, 176,
+                           177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189,
+                           190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202,
+                           203, 204, 205, 206, 207, 208, 209, 210,  45,  46,  47,  48,  49,
+                            50,  51,  52,  53,  54,  55,  57,  60,  62,  64,  66,  70])
+    BOUNDS = [6, 76, 111, 151, 168, 168]
+    VAR_SLICES = {0: slice(0, 640),
+                  3: slice(640, 640+2560),
+                  6: slice(640+2560, 5760)}
+    TIMEPOINTS = OrderedDict(zip(range(-90, 300, 10), range(39)))
+
+    def __init__(self, var=6):
+        self.name = 'hvm'
+        self.var = var
+
+    @property
+    def meta(self):
+        if not hasattr(self, '_meta'):
+            self.fetch()
+            self._meta = pandas.read_pickle(self.datapath('meta'))
+            if self.var is not None:
+                self._meta = self._meta[self._meta['var'] == self.var]
+        return self._meta
+
+    def neural(self, timepoint=None):
+        """
+        Format: (time bins, reps, images, sites)
+        """
+        if timepoint is None:
+            path = self.datapath('neural/averaged/var{}'.format(self.var), 'neural')
+        else:
+            path = self.datapath('neural/temporal/var{}'.format(self.var), 'neural')
+        f = tables.open_file(path)
+        if timepoint is None:
+            nd = f.root.spk[0]  # shape is (1, reps, ...)
+        else:
+            nd = f.root.spk[self.TIMEPOINTS[timepoint]]
+        neural_order = f.root.meta.idx2iid[:]
+        f.close()
+        order = np.array([neural_order.index(n) for n in self.meta.filename])
+        self._neural_data = nd[:, order][:, :, self.IT_NEURONS]
+        return self._neural_data
+
+    def model(self, name='alexnet', layer='pool5'):
+        if name == 'alexnet' and layer == 'pool5':
+            model_feats = np.load(self.datapath('model/alexnet/pool5'))
+        elif name == 'hmo':
+            model_feats = cPickle.load(open(self.datapath('model/hmo/' + layer)))
+        if self.var is not None:
+            model_feats = model_feats[self.VAR_SLICES[self.var]]
+        return model_feats
+
+    def __call__(self, kind='meta', **kwargs):
+        if kind == 'meta':
+            data = self.meta
+        elif kind == 'neural':
+            data = self.neural(**kwargs)
+        elif kind == 'model':
+            data = self.model(**kwargs)
+        return data
+
+    def query(self, query):
+        raise NotImplementedError
+        meta_query = {k:v for k,v in query.items() if '.' not in k}
+
+        keys = {}
+        for key, value in query.items():
+            spl = key.split('.')
+            if len(spl) == 1:
+                spl = ['meta', key]
+            if spl[0] not in keys:
+                keys[spl[0]] = {}
+            keys[spl[0]][spl[1]] = value
+
+        # if 'meta' in keys:
+        #     if 'var' in keys['meta']:
+
+        data = {}
+        for key, query in keys.items():
+            if len(query) > 0:
+                data[key] = getattr(self, key)(**query)
+
+        return data
 
 
 class HvMImageSet(Dataset):
@@ -156,39 +284,66 @@ class HvMImageSet(Dataset):
         return self._meta
 
 
-class HvM6Neural(Dataset):
+class HvMNeural(Dataset):
 
     DATA = {'meta': ('streams/hvm/imageset/meta.pkl', None, 'hvm/imageset/meta.pkl'),
-            'averaged': ('Chabo_Tito_20110907_Var06a_pooled_P58.trim.wh.evoked.repr.h5',
-                         'a95c797b0b2eef56d431c8ccca4c160143a65357', None),
-            'temporal': ('Chabo_Tito_20140307_Var06a_pooled_P58.trim.wh.evoked.repr.h5',
-                         '8ed6ec266fd0104368121aa742038f04681f7231', None),
+
+            'averaged/var0': ('Chabo_Tito_20110907_Var00a_pooled_P58.trim.wh.evoked.repr.h5',
+                              '300b4446797b6244987f3d98b38c4cb36f61086d', None),
+            'averaged/var3': ('Chabo_Tito_20110907_Var03a_pooled_P58.trim.wh.evoked.repr.h5',
+                              'd72802384c35915ceee80a530e1d1053b086975a', None),
+            'averaged/var6': ('Chabo_Tito_20110907_Var06a_pooled_P58.trim.wh.evoked.repr.h5',
+                              'a95c797b0b2eef56d431c8ccca4c160143a65357', None),
+
+            'temporal/var0': ('Chabo_Tito_20140307_Var00a_pooled_P58.trim.wh.evoked.repr.h5',
+                              '588d6d118a45e98c65260e9226c237c72244af0d', None),
+            'temporal/var3': ('Chabo_Tito_20140307_Var03a_pooled_P58.trim.wh.evoked.repr.h5',
+                              '52a35255b1595c29be747c5725be7d6f0e6bd037', None),
+            'temporal/var6': ('Chabo_Tito_20140307_Var06a_pooled_P58.trim.wh.evoked.repr.h5',
+                              '8ed6ec266fd0104368121aa742038f04681f7231', None),
             # 'temporal_raw': 'Chabo_Tito_20140307_Var06a_pooled_P58.trim.raw.d.repr.h5',
             # 'temporal_evoked': 'Chabo_Tito_20140307_Var06a_pooled_P58.trim.evoked.repr.h5'
             }
 
-
-    IT_NEURONS = range(0, 11) + range(45, 85) + range(121, 163) + range(171, 211) + range(221, 256)
+    # ordered as pIT: Chabo, Tito; cIT: Chabo, Tito; aIT: Chabo, Tito (see BOUNDS)
+    IT_NEURONS = np.array([ 10, 121, 122, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134,
+                           135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147,
+                           148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160,
+                           161, 162, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231,
+                           232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244,
+                           245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255,   0,   1,
+                             2,   3,   4,   5,   6,   7,   8,   9,  56,  58,  59,  61,  63,
+                            65,  67,  68,  69,  71,  72,  73,  74,  75,  76,  77,  78,  79,
+                            80,  81,  82,  83,  84, 123, 124, 171, 172, 173, 174, 175, 176,
+                           177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189,
+                           190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202,
+                           203, 204, 205, 206, 207, 208, 209, 210,  45,  46,  47,  48,  49,
+                            50,  51,  52,  53,  54,  55,  57,  60,  62,  64,  66,  70])
+    BOUNDS = [6, 76, 111, 151, 168, 168]
+    VAR_SLICES = {0: slice(0, 640),
+                  3: slice(640, 640+2560),
+                  6: slice(640+2560, 5760)}
     TIMEPOINTS = OrderedDict(zip(range(-90, 300, 10), range(39)))
 
-    def __init__(self):
+    def __init__(self, var=6):
         self.name = 'hvm/neural'
+        self.var = var
 
     @property
     def meta(self):
         if not hasattr(self, '_meta'):
             self.fetch()
             self._meta = pandas.read_pickle(self.datapath('meta'))
-        return self._meta[640 + 2560:]
+        return self._meta[self.VAR_SLICES[self.var]]
 
-    def neural_data(self, timepoint=None):
+    def neural_data(self, var=6, timepoint=None):
         """
         Format: (time bins, reps, images, sites)
         """
         if timepoint is None:
-            path = self.datapath('averaged')
+            path = self.datapath('averaged/var{}'.format(var))
         else:
-            path = self.datapath('temporal')
+            path = self.datapath('temporal/var{}'.format(var))
         f = tables.open_file(path)
         if timepoint is None:
             nd = f.root.spk[0]  # shape is (1, reps, ...)
@@ -225,7 +380,7 @@ class HvM6IT(Dataset):
         self.order = np.array(order)
         self._imageset = HvMImageSet()
         # self.behav = HvMBehav()
-        self._neural = HvM6Neural()
+        self._neural = HvMNeural()
         self._model = HvMModel()
         self.DATA = {}
         self.DATA.update(self._imageset.DATA)
@@ -245,8 +400,8 @@ class HvM6IT(Dataset):
     def meta(self):
         return self._imageset.meta.iloc[640 + 2560 + self.order]
 
-    def neural(self, timepoint=None):
-        d = self._neural.neural_data(timepoint=timepoint)[:, self.order]
+    def neural(self, var=6, timepoint=None):
+        d = self._neural.neural_data(var=var, timepoint=timepoint)[:, self.order]
         neural_data = d[:, :, self._neural.IT_NEURONS]
         return neural_data
 
@@ -299,4 +454,4 @@ def create_tfrec():
 
 
 if __name__ == '__main__':
-    HvM6Neural().neural_data()
+    HvM()._upload('meta.pkl')
